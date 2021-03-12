e42bda2010911a659bcfa02851f651871307fca5
Mritunjoy Mahanta <mritunjoy.mahanta001@gmail.com>
2021-03-12 23:34:53 +0530

    added the todo functionality

--- src/App.js
+++ src/App.js
@@ -3,11 +3,24 @@ import "./App.css";
 
 function App() {
   const [todos, setTodos] = useState(["Eat", "Sleep", "Code", "Repeat"]);
+  const [input, setInput] = useState("");
+
+  const addTodo = (e) => {
+    // set the input to the todos Array
+    e.preventDefault(); //preventing to refresh the page when submitting
+    setTodos([...todos, input]);
+    setInput("");
+  };
+
   return (
     <div className="App">
       <h1>Hello world</h1>
-      <input type="text" />
-      <button>Add</button>
+      <form>
+        <input value={input} onChange={(e) => setInput(e.target.value)} />
+        <button type="submit" onClick={addTodo}>
+          Add
+        </button>
+      </form>
 
       <ul>
         {todos.map((todo) => (
